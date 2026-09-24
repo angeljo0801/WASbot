83486a11da5ab0a38303f9d0e3047bf48931df2d
angeljo0801 <angeljo0801@gmail.com>
2026-09-23 23:23:36 -0400

    Clean WhatsBot backup analyzer warning

--- mobile/lib/services/backup_service.dart
+++ mobile/lib/services/backup_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:archive/archive.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
