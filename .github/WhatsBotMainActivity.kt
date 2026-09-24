cab349b4fbf5b744d3d170f8159687de22d79670
angeljo0801 <angeljo0801@gmail.com>
2026-09-23 23:21:26 -0400

    Register WhatsBot backup bridge

---
+++ .github/WhatsBotMainActivity.kt
@@ -0,0 +1,14 @@
+package com.whatsbot.whatsbot
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        WhatsBotBackupStorageBridge.register(
+            applicationContext,
+            flutterEngine.dartExecutor.binaryMessenger
+        )
+    }
+}
