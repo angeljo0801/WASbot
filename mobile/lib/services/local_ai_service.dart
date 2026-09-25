56d4dd4258913595b36ce1a92c637b374a63cfcf
angeljo0801 <angeljo0801@gmail.com>
2026-09-24 23:15:26 -0400

    Keep Remesas as protected automatic category

--- mobile/lib/services/local_ai_service.dart
+++ mobile/lib/services/local_ai_service.dart
@@ -273,7 +273,7 @@ class LocalAiService {
     }
     const categories = {
       'Inbox', 'Clientes', 'Trabajo', 'Personal', 'Compras', 'Gastos',
-      'Ideas', 'Documentos', 'Recordatorios', 'Fotos', 'Remesas'
+      'Ideas', 'Documentos', 'Recordatorios', 'Fotos'
     };
     final rawCategory = (data['category'] ?? note.category).toString();
     final category = note.messageType == 'contact'
